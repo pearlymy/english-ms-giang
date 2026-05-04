3bb049c2ed09b8c51259c82ec6c55d7856ed0b8d
Hieu Phan <pearlymy2710@gmail.com>
2026-05-04 21:50:57 +0700

    fix: add basename to BrowserRouter for GitHub Pages sub-path

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import { UserManagementProvider } from './contexts/UserManagementContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <AuthProvider>
         <TeacherProvider>
           <UserManagementProvider>
